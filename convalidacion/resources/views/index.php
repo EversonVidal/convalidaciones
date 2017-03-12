f808b35b1e4d44c9e96fab2fca568bdb2ae6f0fa
Everson Vidal <flextoo@hotmail.com>
2017-03-12 17:01:23 -0400

    CONVALIDACIONES-UAB
    Sistema web de conavlidaciones de estudiantes UAB en
    php-mysql-laravel-html5

---
+++ convalidacion/resources/views/index.php
@@ -0,0 +1,44 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ribbon Login Form Flat Responsive widget Design :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<meta name="keywords" content="Ribbon Login Form Responsive Templates, Iphone Compatible Templates, Smartphone Compatible Templates, Ipad Compatible Templates, Flat Responsive Templates"/>
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!--webfonts-->
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' rel='stylesheet' type='text/css'>
+<!--/webfonts-->
+</head>
+<body>
+<!--start-main-->
+<h1>Bienvenido!<span>Logueate por favor...</span></h1>
+<div class="login-box">
+		<form>
+			<input type="text" class="text" value="Nombre de Usuario" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Username';}" >
+			<input type="password" value="password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'password';}">
+		</form>
+		<div class="remember">
+			
+			<h4>Olvidaste tu contraseña?<a href="#">Click aqui.</a></h4>
+		</div>
+		<div class="clear"> </div>
+		<div class="btn">
+			<input type="submit" value="Iniciar Sesion" >
+		</div>
+		<div class="clear"> </div>
+</div>
+<!--//End-login-form-->
+<!--start-copyright-->
+<div class="copy-right">
+	<p>Realizado por <a href="http://w3layouts.com">VIDAL</a></p> 
+</div>
+<!--//end-copyright-->		
+</body>
+</html>
